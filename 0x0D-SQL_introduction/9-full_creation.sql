d507c764a01a6336a236a12e150a7b26cf278a7d
4honore <ishimwehonore450@gmail.com>
2023-03-14 11:13:08 -0700

    create another table with its values

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows
+--The database name will be passed as an argument to the mysql command
+--You are not allowed to use the SELECT and SHOW statements
+--Your script should create these records:
+CREATE TABLE second_table(id INT, name VARCHAR(256), score INT) VALUES(1, "John", 10),(2, "Alex", 3),(3, "Bob", 14),(4, "George" ,8);
+
