34267516b5ff65db14b6db4deffdd524af2b74bd
4honore <ishimwehonore450@gmail.com>
2023-03-14 07:27:30 -0700

    shw table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -2,5 +2,5 @@
 --The database name will be passed as an argument of the mysql command
 --If the table first_table already exists, your script should not fail
 --You are not allowed to use the SELECT or SHOW statements
-CREATE TABLE first_table(id(INT),name(VARCHAR)) IF NOT EXISTS;
+CREATE TABLE first_table(id INT,name VARCHAR(256)) IF NOT EXISTS;
 
